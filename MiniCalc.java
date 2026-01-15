7c6063cd9d28ac8044f3548e353fb689b4ba3094
Ekis2u <ekisdobleugames@gmail.com>
2026-01-15 10:45:12 +0100

    fix: hotfix add multiply

--- MiniCalc.java
+++ MiniCalc.java
@@ -2,4 +2,5 @@
 public class MiniCalc {
 	public static int add(int a, int b) { return a + b; }
 	public static int subtract(int a, int b) { return a - b; }
+	public static int multiply(int a, int b) { return a * b; }
 }
